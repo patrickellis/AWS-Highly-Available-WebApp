0df9cb822a66905a964af310b4f59f1e9412d97e
patrickellis <patrick.ellis.w1@gmail.com>
2022-09-11 16:34:59 +0100

    fix: typo ww -> www to target apache2 files correctly

--- terraform/modules/aws-launch-template/user_data.sh
+++ terraform/modules/aws-launch-template/user_data.sh
@@ -6,4 +6,4 @@ systemctl start apache2
 mkdir /tmp/portfolio
 cd /tmp/portfolio && git clone https://github.com/patrickellis/Portfolio
 cp -rf Portfolio/* /var/www/html/
-sed -i "s/placeholder-header/$(hostname -f)/g" /var/ww/html/all/menu.html
+sed -i "s/placeholder-header/$(hostname -f)/g" /var/www/html/all/menu.html
